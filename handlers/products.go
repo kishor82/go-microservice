e2fa3ea46a46661e9ff6c42af94c206cdb4c91ed
kishor82 <kishorrathva8298@gmail.com>
2023-11-16 20:16:03 +0530

    - Removed hello world endpoints - Added products dummy json - Added getProducts GET endpoints (JSON)

---
+++ handlers/products.go
@@ -0,0 +1,24 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/kishor82/go-microservice/data"
+)
+
+type Products struct {
+	l *log.Logger
+}
+
+func NewProducts(l *log.Logger) *Products {
+	return &Products{l}
+}
+
+func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	lp := data.GetProducts()
+	err := lp.ToJSON(rw)
+	if err != nil {
+		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
+	}
+}
