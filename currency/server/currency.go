b1c9b3e49094c14e0cee06b1efc1f88e083d088c
kishor82 <kishorrathva8298@gmail.com>
2023-11-27 20:45:19 +0530

    Added grpc error handling in biderectional method

--- currency/server/currency.go
+++ currency/server/currency.go
@@ -49,7 +49,17 @@ func (c *Currency) handleUpdates() {
 						rr.GetDestination().String(),
 					)
 				}
-				err = k.Send(&cp.RateResponse{Base: rr.Base, Destination: rr.Destination, Rate: r})
+				err = k.Send(
+					&cp.StreamingRateResponse{
+						Message: &cp.StreamingRateResponse_RateResponse{
+							RateResponse: &cp.RateResponse{
+								Base:        rr.Base,
+								Destination: rr.Destination,
+								Rate:        r,
+							},
+						},
+					},
+				)
 				if err != nil {
 					c.log.Error(
 						"Unable to send updated reate",
@@ -68,6 +78,7 @@ func (c *Currency) GetRate(ctx context.Context, rr *cp.RateRequest) (*cp.RateRes
 	c.log.Info("GetRate", "base", rr.GetBase(), "destination", rr.GetDestination())
 
 	if rr.Base == rr.Destination {
+		// create the grpc error and return to the client
 		err := status.Newf(
 			codes.InvalidArgument,
 			"Base currency %s can not be same as destination currency %s",
@@ -111,6 +122,47 @@ func (c *Currency) SubscribeRates(src cp.Currency_SubscribeRatesServer) error {
 		rrs, ok := c.subscriptions[src]
 		if !ok {
 			rrs = []*cp.RateRequest{}
+		}
+
+		// check that subscription does not exists
+		var validationError *status.Status
+		for _, v := range rrs {
+			if v.Base == rr.Base && v.Destination == rr.Destination {
+				// subscription exists return errors
+				// NOTE: here bymistake i created newVariable with := which override the aboce declared validationError due to scope :(
+				// INFO:  In Go, if you declare a variable with the same name inside a block (such as the for loop in your case), it will create a new variable with that name, shadowing any variables with the same name in the outer scope.
+				// to prevent this in future i added "analyses": {"shadow": true} setting in gopls lsp config.
+				validationError = status.Newf(
+					codes.AlreadyExists,
+					"Unable to subscribe for currency as subscription already exist",
+				)
+				// add the original request as metadata
+				validationError, err = validationError.WithDetails(rr)
+				if err != nil {
+					c.log.Error("Unable to add metadata to error", "error", err)
+					break
+				}
+				break
+			}
+		}
+		if validationError != nil {
+			err := src.Send(
+				&cp.StreamingRateResponse{
+					Message: &cp.StreamingRateResponse_Error{
+						Error: validationError.Proto(),
+					},
+				},
+			)
+			if err != nil {
+				c.log.Error(
+					"Unable to send error",
+					"base",
+					rr.GetBase().String(),
+					"destination",
+					rr.GetDestination().String(),
+				)
+			}
+			continue
 		}
 
 		rrs = append(rrs, rr)
